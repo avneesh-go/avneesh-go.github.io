045d2c75d7b490796c329b87430714d6fa0b0035
Avneesh Jagtap <exchangeinterworld@gmail.com>
2025-02-06 11:12:16 +0530

    delete files

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const toggleButton = document.getElementById("theme-toggle");
-    const body = document.body;
-
-    toggleButton.addEventListener("click", function () {
-        body.classList.toggle("dark-mode");
-        toggleButton.textContent = body.classList.contains("dark-mode") ? "☀️" : "🌙";
-    });
-});
